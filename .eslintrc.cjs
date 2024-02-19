7b30b0090a6277023cdcf0e9b6d7cb3c01aef2da
Merce Casamitjana <casamitjana.agraz@gmail.com>
2024-02-19 17:22:30 +0100

    Add parseOptions to eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
@@ -16,10 +22,10 @@ module.exports = {
     ],
     'quotes': ['error', 'single'],
     // we want to force semicolons
-    'semi': ['error', 'always'],
+    'semi': ['error', 'never'],
     // we use 2 spaces to indent our code
     'indent': ['error', 2],
     // we want to avoid extraneous spaces
     'no-multi-spaces': ['error']
-  },
+  }
 }
